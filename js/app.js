49d519b982d60cf24b1c603acc60f803e0b23c22
Rodrigaumm <rodrigo@arch.localdomain>
2020-07-20 01:55:48 -0300

    New Unfinished Github.io Homepage

---
+++ js/app.js
@@ -0,0 +1,42 @@
+const getRequestGithub = new Request("https://api.github.com/repos/Rodrigaumm/rodrigaumm.github.io/contents/Projects",{
+	method:"GET"
+})
+
+let githubData = [];
+const section = document.querySelector('.github-links');
+const imgSrcs = ['./imgs/flexblog.png','./imgs/wildbeast.png']
+
+fetch(getRequestGithub)
+.then((response)=>{
+	return response.json()
+})
+.then((arr)=>{
+	githubData = arr;
+	githubData.forEach((content,index)=>{
+		const link = document.createElement('a')
+		const div = document.createElement('div')
+		const img = document.createElement('img')
+		
+		div.classList.add('project-container')
+
+		img.src=imgSrcs[index]
+		img.alt=content.name
+		img.classList.add('project-print')
+
+		link.classList.add('project-link')
+		link.href = `https://rodrigaumm.github.io/Projects/${content.name}/index.html`
+		link.innerHTML += content.name
+
+		
+		link.appendChild(img)
+		div.appendChild(link)
+		section.appendChild(div)
+	})
+	console.log(githubData)
+})
+.catch((error)=>{
+	console.error(error)
+})
+
+
+
