30a1602aff5fafcb0ad05067cd4408dbc16658f5
Rodrigaumm <chaveiro4590@gmail.com>
2021-02-22 13:36:53 -0300

    Adds github-explorer project to Projects folder

---
+++ Projects/githubExplorer/precache-manifest.38a2238b7d5eb456be9e93760e12d50b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8675427705921203204962e4f7165bdb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "739afc3aac34d50837fe",
+    "url": "/static/js/2.9139910c.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.9139910c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35f8ec63e091f32c9aaf",
+    "url": "/static/js/main.8a3d7f66.chunk.js"
+  },
+  {
+    "revision": "50002dd3f47e80e40deb",
+    "url": "/static/js/runtime-main.c9d702bc.js"
+  },
+  {
+    "revision": "88c2d35eff7ebd12dc82cba71327f4bf",
+    "url": "/static/media/github-background.88c2d35e.svg"
+  },
+  {
+    "revision": "18e060ecceda949068ce519ae6d8b261",
+    "url": "/static/media/logo.18e060ec.svg"
+  }
+]);
